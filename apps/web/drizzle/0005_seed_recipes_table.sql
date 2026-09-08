277c6c24be940e8a94ad42c3c9b9f51c3237ada3
khlav <5116091+khlav@users.noreply.github.com>
2026-09-08 09:53:55 -0400

    recipes: seed Sulfuron Hammer recipe (#126)
    * feat(recipes): seed Sulfuron Hammer recipe
    
    * fix(recipes): add bwl/mc, weapon, component tags to Sulfuron Hammer

--- apps/web/drizzle/0005_seed_recipes_table.sql
+++ apps/web/drizzle/0005_seed_recipes_table.sql
@@ -111,5 +111,6 @@ VALUES
     (20029, NULL, 'Enchanting', 'Enchant Weapon - Icy Chill', FALSE, 'For Coldrage Daggers on Viscidous', ARRAY['aq40', 'melee']),
     (20853, 16982, 'Leatherworking', 'Corehound Boots', FALSE, 'Fire resist boots for Feral Druids and Rogues in BWL/MC', ARRAY['feet', 'bwl/mc', 'melee', 'fireresist']),
     (23707, 19149, 'Leatherworking', 'Lava Belt', FALSE, 'Fire resist belt for Feral Druids and Rogues in BWL/MC', ARRAY['waist', 'bwl/mc', 'melee', 'fireresist']),
-    (20854, 16983, 'Leatherworking', 'Molten Helm', FALSE, 'Fire resist helm for Feral Druids and Rogues in BWL/MC', ARRAY['head', 'bwl/mc', 'melee', 'fireresist'])
+    (20854, 16983, 'Leatherworking', 'Molten Helm', FALSE, 'Fire resist helm for Feral Druids and Rogues in BWL/MC', ARRAY['head', 'bwl/mc', 'melee', 'fireresist']),
+    (21161, 17193, 'Blacksmithing', 'Sulfuron Hammer', FALSE, 'Combines with Eye of Sulfuras to create Sulfuras, Hand of Ragnaros', ARRAY['bwl/mc', 'weapon', 'component'])
 ON CONFLICT (recipe_spell_id) DO NOTHING;
